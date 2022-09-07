ebacb018601d13f214db72b55590489eae196fba
SantiTorija <torija.santiago@gmail.com>
2022-09-07 08:51:38 -0300

    admin schema y seeder

---
+++ models/Administrator.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const adminSchema = new Schema({
+  firstname: {
+    type: String,
+    required: "First name is required",
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    required: "Last name is required",
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: "Password is required",
+    trim: true,
+    minLength: 8,
+    select: false,
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+    validate: [validateEmail, "Please fill a valid email address"],
+  },
+});
+
+adminSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+  return next();
+});
+
+const Administrator = mongoose.model("Administrator", adminSchema);
+
+module.exports = Administrator;
--- models/index.js
+++ models/index.js
@@ -6,10 +6,12 @@ const User = require("./User");
 const Wine = require("./Wine");
 const Order = require("./Order");
 const Variety = require("./Variety");
+const Administrator = require("./Administrator");
 
 module.exports = {
   User,
   Wine,
   Order,
   Variety,
+  Administrator,
 };
