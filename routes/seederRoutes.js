d2db1e3560bc3f14c0c115758ca6cf92b52e903f
SantiTorija <torija.santiago@gmail.com>
2022-09-20 19:15:03 -0300

    arreglando seeder

--- routes/seederRoutes.js
+++ routes/seederRoutes.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const seederRoutes = express.Router();
-const seederController = require("../controllers/seederController");
+const { mongoose } = require("../dbInitialSetup");
+const seeders = require("../seeders");
 
-seederRoutes.get("/", seederController.destroy);
+seederRoutes.get("/", async function (req, res) {
+  await mongoose.connection.dropCollection("users");
+  await mongoose.connection.dropCollection("administrators");
+  await mongoose.connection.dropCollection("wines");
+  await mongoose.connection.dropCollection("types");
+  await mongoose.connection.dropCollection("orders");
+  seeders();
+  res.json({ response: "base de datos reseteada" });
+});
 
 module.exports = seederRoutes;
