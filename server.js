73e46fd4e36024cbe07ac24067f8e3b89d60b1b7
alankatz3793 <alankatz2000@gmail.com>
2022-09-20 20:49:59 -0300

    cambios back

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 routes(app);
 
 //seeders(); // Crea tablas e inserta datos de prueba.
