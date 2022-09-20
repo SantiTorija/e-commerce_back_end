2a05eaeb88f706f6ba98933da8cd9a929897f5f0
alankatz3793 <alankatz2000@gmail.com>
2022-09-20 20:24:33 -0300

    reset

--- controllers/seederController.js
+++ controllers/seederController.js
@@ -1,19 +1,19 @@
 const { mongoose } = require("../dbInitialSetup");
 const seeders = require("../seeders");
+const Admin = require("../models/Administrator");
 const Wine = require("../models/Wine");
 const Type = require("../models/Type");
 const User = require("../models/User");
 const Order = require("../models/Order");
-const Admin = require("../models/Administrator");
 
 module.exports = {
   destroy: async function (req, res) {
     try {
-      Order.remove({}, function (err) {});
-      Wine.remove({}, function (err) {});
-      Type.remove({}, function (err) {});
-      User.remove({}, function (err) {});
-      Admin.remove({}, function (err) {});
+      await Order.remove({});
+      await Wine.remove({});
+      await Type.remove({});
+      await User.remove({});
+      await Admin.remove({});
       await seeders();
       res.json({ response: "base de datos reseteada" });
     } catch (e) {
